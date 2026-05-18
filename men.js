fd32e1ce8a622d3efa53c1c8c33449523881a437
walissonsilvaribeiro-ent <walisson.silva.ribeiro@escola.pr.gov.br>
2026-05-18 09:27:07 -0300

    atulização dia 18 do 05

--- men.js
+++ men.js
@@ -1,5 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i ,botoes.length;(++){
-    console.log(i);
+    botoes[i].onclick = function(){
+   
+   for(let j=0<botoes.lenght;i++){
+        botoes[j].classList.remove("ativo");
+    }
+
+    botoes{i}.classList.add("ativo");
 }
